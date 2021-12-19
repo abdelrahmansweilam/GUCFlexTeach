3880118ca6c3be921872b01af155111f8bae26ba
Baggy269 <74379635+Baggy269@users.noreply.github.com>
2021-12-19 10:25:57 +0200

    Update profile_screen.dart

--- lib/Routes/profile_screen.dart
+++ lib/Routes/profile_screen.dart
@@ -11,7 +11,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
   var name = "name";
   var username = "user123";
   var id = "43-1111";
-  Future<void> getUserInfo() async {}
+  var isLoading = true;
+  Future<void> getUserInfo() async {
+    //await get info from db
+    isLoading = false;
+  }
 
   @override
   void initState() {
@@ -22,37 +26,43 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
-      const Align(
-          alignment: Alignment.center,
-          child: CircleAvatar(
-            radius: 100,
-            // user's profile pic if present
-            //foregroundImage: NetworkImage(userAvatarUrl),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                padding:
+                    EdgeInsets.only(top: 50, left: 10, right: 10, bottom: 10),
+                alignment: Alignment.center,
+                child: Column(children: [
+                  const CircleAvatar(
+                    radius: 100,
+                    // user's profile pic if present
+                    //foregroundImage: NetworkImage(userAvatarUrl),
 
-            // generic avatar otherwise
-            backgroundImage: NetworkImage(
-                "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+                    // generic avatar otherwise
+                    backgroundImage: NetworkImage(
+                        "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
 
-            // alternatively, the user's intials could be used if they don't have a profile pic
-            //backgroundColor: Colors.grey.shade800,
-            //child: const Text('AH'),
-          )),
-      Text("Name: " + name,
-          softWrap: true,
-          overflow: TextOverflow.fade,
-          style: TextStyle(fontSize: 30),
-          textAlign: TextAlign.left),
-      Text("Username: " + username,
-          softWrap: true,
-          overflow: TextOverflow.fade,
-          style: TextStyle(fontSize: 30),
-          textAlign: TextAlign.left),
-      Text("ID: " + id,
-          softWrap: true,
-          overflow: TextOverflow.fade,
-          style: TextStyle(fontSize: 30),
-          textAlign: TextAlign.left)
-    ]));
+                    // alternatively, the user's intials could be used if they don't have a profile pic
+                    //backgroundColor: Colors.grey.shade800,
+                    //child: const Text('AH'),
+                  ),
+                  Text("Name: " + name,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(fontSize: 30),
+                      textAlign: TextAlign.left),
+                  Text("Username: " + username,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(fontSize: 30),
+                      textAlign: TextAlign.left),
+                  Text("ID: " + id,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(fontSize: 30),
+                      textAlign: TextAlign.left)
+                ])));
   }
 }
