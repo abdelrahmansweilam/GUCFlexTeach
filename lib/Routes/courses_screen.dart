7c200f1adfc1a9eaae3eba01b3466c7533beb8e2
Baggy269 <74379635+Baggy269@users.noreply.github.com>
2021-12-24 14:55:15 +0200

    Update courses_screen.dart

--- lib/Routes/courses_screen.dart
+++ lib/Routes/courses_screen.dart
@@ -69,3 +69,60 @@ class _CourseCardState extends State<CourseCard> {
     );
   }
 }
+
+class CourseScreen extends StatefulWidget {
+  @override
+  State<CourseScreen> createState() => _CourseScreenState();
+}
+
+class _CourseScreenState extends State<CourseScreen> {
+  List<String> instructors = [];
+  var discussions;
+  Future<void> getCourseInfo() async {
+    //await course info from DB
+    instructors = ["Dr. x", "TA y"];
+    discussions = [];
+  }
+
+  @override
+  void initState() {
+    getCourseInfo();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final courseName = routeArgs["course"];
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(courseName!),
+        ),
+        body: Column(
+          children: [
+            const Text("Instructors",
+                softWrap: true,
+                overflow: TextOverflow.fade,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center),
+            ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemBuilder: (ctx, index) {
+                return Text(instructors[index],
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                    style: TextStyle(color: Colors.black, fontSize: 30),
+                    textAlign: TextAlign.left);
+              },
+              itemCount: instructors.length,
+            ),
+          ],
+        ));
+  }
+}
