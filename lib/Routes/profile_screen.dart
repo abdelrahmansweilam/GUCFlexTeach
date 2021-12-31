c454b3c4cdf3ba17f232a70ce16da8e426bd8746
Baggy269 <74379635+Baggy269@users.noreply.github.com>
2021-12-31 16:55:58 +0200

    added provider

--- lib/Routes/profile_screen.dart
+++ lib/Routes/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'dart:io';
+import 'package:provider/provider.dart';
+import 'package:flexteach/Providers/user_info_provider.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -10,66 +11,90 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  var name = "name";
-  var username = "user123";
-  var id = "43-1111";
-  var isLoading = true;
-
-  Future<void> getUserInfo() async {
-    //await get info from db
-    isLoading = false;
-  }
-
-  @override
-  void initState() {
-    getUserInfo();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final userInfoProvider = Provider.of<UserInfoProvider>(context);
+    final appID = userInfoProvider.getID;
+    final coursesList = userInfoProvider.getCourses;
+    final major = userInfoProvider.getMajor;
+    final name = userInfoProvider.getName;
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text("Profile"),
         ),
-        body: isLoading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Container(
-                padding:
-                    EdgeInsets.only(top: 50, left: 10, right: 10, bottom: 10),
-                alignment: Alignment.center,
-                child: Column(children: [
-                  const CircleAvatar(
-                    radius: 100,
-                    // user's profile pic if present
-                    //foregroundImage: NetworkImage(userAvatarUrl),
+        body: Container(
+            padding: EdgeInsets.only(top: 50, left: 10, right: 10, bottom: 10),
+            alignment: Alignment.center,
+            child: Column(children: [
+              const CircleAvatar(
+                radius: 100,
+                // user's profile pic if present
+                //foregroundImage: NetworkImage(userAvatarUrl),
 
-                    // generic avatar otherwise
-                    backgroundImage: NetworkImage(
-                        "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
+                // generic avatar otherwise
+                backgroundImage: NetworkImage(
+                    "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"),
 
-                    // alternatively, the user's intials could be used if they don't have a profile pic
-                    //backgroundColor: Colors.grey.shade800,
-                    //child: const Text('AH'),
+                // alternatively, the user's intials could be used if they don't have a profile pic
+                //backgroundColor: Colors.grey.shade800,
+                //child: const Text('AH'),
+              ),
+              Text("Name: " + name,
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.left),
+              Text("ID: " + appID,
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.left),
+              Text("Major: " + major,
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.left),
+              Container(
+                margin: EdgeInsets.all(10),
+                width: double.infinity,
+                child: const Text(
+                  "Courses",
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text("Name: " + name,
-                      softWrap: true,
-                      overflow: TextOverflow.fade,
-                      style: TextStyle(fontSize: 30),
-                      textAlign: TextAlign.left),
-                  Text("Username: " + username,
-                      softWrap: true,
-                      overflow: TextOverflow.fade,
-                      style: TextStyle(fontSize: 30),
-                      textAlign: TextAlign.left),
-                  Text("ID: " + id,
-                      softWrap: true,
-                      overflow: TextOverflow.fade,
-                      style: TextStyle(fontSize: 30),
-                      textAlign: TextAlign.left)
-                ])));
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              ListView.separated(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(
+                  height: 1,
+                  thickness: 2,
+                ),
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    title: Text(
+                      coursesList[index],
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushNamed("/courseDiscussionsRoute",
+                          arguments: {"course": coursesList[index]});
+                    },
+                    trailing: Platform.isIOS
+                        ? Icon(Icons.arrow_forward_ios)
+                        : Icon(Icons.arrow_forward),
+                  );
+                },
+                itemCount: coursesList.length,
+              ),
+            ])));
   }
 }
