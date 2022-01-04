25f13ad861f6a4555f360d4661efe195e071a96f
reeem.salah <reem.mansour@levelset.com>
2022-01-04 11:43:57 +0200

    Adding to calendar

--- lib/Routes/deadlines_screen.dart
+++ lib/Routes/deadlines_screen.dart
@@ -2,6 +2,7 @@ import 'package:flexteach/Backend/notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:add_2_calendar/add_2_calendar.dart';
 
 import '../Providers/user_info_provider.dart';
 import '../Models/deadline.dart';
@@ -47,23 +48,39 @@ class _DeadlinesScreenState extends State<DeadlinesScreen> {
           child: ListView.builder(
             shrinkWrap: true,
             itemBuilder: (ctx, index) {
-              return Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                child: ListTile(
-                  leading: assignment_icon,
-                  trailing: Text(
-                    deadlines[index].course_code,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  title: Text(
-                    deadlines[index].title,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    "Due on " +
-                        DateFormat('dd MMM yyyy kk:mm a')
-                            .format(deadlines[index].deadline_date.toDate()),
+              return InkWell(
+                onDoubleTap: () {
+                  final Event event = Event(
+                    title: deadlines[index].course_code,
+                    description: deadlines[index].title,
+                    startDate: deadlines[index].deadline_date.toDate(),
+                    endDate: deadlines[index].deadline_date.toDate(),
+                  //TODO Add reminder in IOS
+                    // iosParams: IOSParams(
+                    //   reminder: Duration(
+                    //       /* Ex. hours:1 */), // on iOS, you can set alarm notification after your event.
+                    // ),
+                  );
+                  Add2Calendar.addEvent2Cal(event);
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: ListTile(
+                    leading: assignment_icon,
+                    trailing: Text(
+                      deadlines[index].course_code,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    title: Text(
+                      deadlines[index].title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "Due on " +
+                          DateFormat('dd MMM yyyy kk:mm a')
+                              .format(deadlines[index].deadline_date.toDate()),
+                    ),
                   ),
                 ),
               );
